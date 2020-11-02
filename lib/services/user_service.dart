b41c1ed5acf1a66af4e818805e512af7384b257c
leodelgadodev <leodelgado.dev@gmail.com>
2020-11-01 22:01:11 -0300

    userservice

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,9 +1,11 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geo_stories/models/user_dto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserService {
   static FirebaseAuth auth = FirebaseAuth.instance;
+  static FirebaseFirestore database = FirebaseFirestore.instance;
 /*FirebaseAuth.instance
   .authStateChanges()
   .listen((User user) {
@@ -21,14 +23,21 @@ class UserService {
     );
   }
 
-  static Future<void> register() async{
+  static Future<void> register(String email, String password, String userName) async{
     UserCredential userCredential = await auth.createUserWithEmailAndPassword(
-        email: "woody.allen@example.com",
-        password: "SuperSecretPassword!"
+        email: email,
+        password: password
     );
-    return userCredential;
+
+    String userID = userCredential.user.uid;
+
+    await database.collection("users")
+        .doc(userID)
+        .set({
+          'username': userName,
+          'avatar': ''
+        });
+
+    return userID;
   }
-
-
-
 }
