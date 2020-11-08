c0475e8b021ebfc4480dbd0e4549a85bf37d786d
w-etc <34588902+w-etc@users.noreply.github.com>
2020-11-08 20:19:34 -0300

    [Fix] Tests en Travis (#17)
    * cambiando versiones para evitar errores
    
    * arreglando tests
    
    * arreglando tests (esta vez es en serio)

--- lib/components/marker_icon.dart
+++ lib/components/marker_icon.dart
@@ -44,7 +44,7 @@ class MarkerIcon extends StatelessWidget {
                   color: isLiked ? Colors.red[800] : Colors.grey,
                 );
               },
-              likeCount: markerDTO.likes.length,
+              likeCount: markerDTO.likes?.length,
               isLiked: _userLikedIt(),
             ),
             IconButton(
@@ -70,9 +70,7 @@ class MarkerIcon extends StatelessWidget {
     );
   }
   bool _userLikedIt() {
-    String uid = UserService.getCurrentUser().uid;
-    print(uid);
-    print(markerDTO.likes.contains("asd1"));
-    return markerDTO.likes.contains(uid);
+    final user = UserService.getCurrentUser();
+    return user != null &&  markerDTO.likes?.contains(user.uid);
   }
 }
