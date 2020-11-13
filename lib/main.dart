b906646bec9fa5423a066ffa873874c9d9c85097
ItuFede <federicoituarte1@gmail.com>
2020-11-12 21:53:42 -0300

    disconnectUser + handle generic error

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:geo_stories/screens/welcome_page.dart';
 
+import 'constants.dart';
+
 Future<void> main() async {
   //debugPaintSizeEnabled = true;
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +13,27 @@ Future<void> main() async {
   await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoder, 'assets/icons/geostories-logo.svg'), null);
   runApp(MyApp());
 }
+
+  Widget buildError(BuildContext context, FlutterErrorDetails error) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      body: Column(
+        children: [
+          SizedBox(height: 100.0),
+          Image.asset(kErrorFlutter, scale: 5,),
+          SizedBox(height: 50.0),
+          Center(
+            child: Text("Se ha producido un error inesperado. 😨", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.red),),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: Text(error.exceptionAsString(), style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12, color: Colors.white),),
+          ),
+          //Image.asset('images/pic3.jpg'),
+        ],
+      ),
+    );
+  }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -22,7 +45,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-
+      builder: (BuildContext context, Widget widget) {
+        ErrorWidget.builder = (FlutterErrorDetails errorDetails) {
+          return buildError(context, errorDetails);
+        };
+        return widget;
+      },
       home: WelcomePage(),
     );
   }
