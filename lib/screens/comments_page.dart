fd473584d939f42c625ea1be9735cd59fdc36044
leodelgadodev <leodelgado.dev@gmail.com>
2020-11-21 20:48:46 -0300

    avances con lista de comments

--- lib/screens/comments_page.dart
+++ lib/screens/comments_page.dart
@@ -35,7 +35,6 @@ class CommentsPageState extends State<CommentsPage> {
   }
 
   Widget getCommentListFromMarker(DocumentSnapshot markerDoc) {
-
     var markerData = markerDoc.data();
     MarkerDTO _markerDTO = MarkerDTO.fromJSON(markerData, markerDTO.id);
     List<CommentDTO> _comments = _markerDTO.comments.map((comment) =>
