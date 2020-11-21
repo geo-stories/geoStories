3f45aa15907bef91ecc1ff0daaef8842bd432565
Nahue507 <nahuelggt@gmail.com>
2020-11-21 19:44:38 -0300

    Fix with _buildCommentItem

--- lib/screens/comments_page.dart
+++ lib/screens/comments_page.dart
@@ -36,6 +36,7 @@ class CommentsPageState extends State<CommentsPage> {
   }
 
   Widget getCommentListFromMarker(DocumentSnapshot markerDoc) {
+
     var markerData = markerDoc.data();
     MarkerDTO _markerDTO = MarkerDTO.fromJSON(markerData, markerDTO.id);
     List<CommentDTO> _comments = _markerDTO.comments.map((comment) =>
@@ -175,7 +176,7 @@ class CommentsPageState extends State<CommentsPage> {
         builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.hasData) {
             var marker = snapshot.data;
-            return getCommentListFromMarker(marker);
+            return _buildCommentsList(marker);
           }
           return Container();
         },
