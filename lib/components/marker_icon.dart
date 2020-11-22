413f05a47e198daa766ca5093667b3f5b680b6a3
ItuFede <federicoituarte1@gmail.com>
2020-11-21 21:59:49 -0300

    fix merge

--- lib/components/marker_icon.dart
+++ lib/components/marker_icon.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:geo_stories/models/marker_dto.dart';
 import 'package:geo_stories/screens/comments_page.dart';
@@ -9,34 +6,11 @@ import 'package:geo_stories/services/marker_service.dart';
 import 'package:geo_stories/services/user_service.dart';
 import 'package:like_button/like_button.dart';
 
-import '../constants.dart';
-
-class MarkerIcon extends StatefulWidget  {
+class MarkerIcon extends StatelessWidget {
   final MarkerDTO markerDTO;
-
-  MarkerIcon({this.markerDTO});
-
-  @override
-  _MarkerIconState createState() => _MarkerIconState();
-}
-
-class _MarkerIconState extends State<MarkerIcon>{
-  int counterComents;
-
-  Widget editButton(){
-    if(UserService.isMarkerOwner(widget.markerDTO.owner)){
-      return IconButton(
-        key: ValueKey("EditButton"),
-        icon : Icon(Icons.edit_outlined),
-        color: Colors.black,
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context){
-            return EditMarker(widget.markerDTO);
-          }));
-        },
-      );
-    }
-  }
+  const MarkerIcon({
+    Key key, this.markerDTO
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -45,52 +19,85 @@ class _MarkerIconState extends State<MarkerIcon>{
       iconSize: 50.0,
       color: Colors.red,
       onPressed: () {
-        _showMarkerModalInfo(context);
+        _MarkerModalInfo(context);
       },
     );
   }
 
-  Future<void> _showMarkerModalInfo(BuildContext context) async {
+  Future<void> _MarkerModalInfo(BuildContext context) async {
     showDialog(
         context: context,
         child: new AlertDialog(
           backgroundColor: Colors.white70,
-          title: Text(widget.markerDTO.title, textAlign: TextAlign.center,),
+          title: Text(markerDTO.title, textAlign: TextAlign.center,),
           content: RichText(
             text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: ' •' + await MarkerService.GetOwnerUsername(widget.markerDTO.owner) + '• ', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                  TextSpan(text: widget.markerDTO.description, style: TextStyle(color: Colors.grey[900])),
-                ],
-              ),
+              children: <TextSpan>[
+                TextSpan(text: ' •' + await MarkerService.GetOwnerUsername(markerDTO.owner) + '• ', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                TextSpan(text: markerDTO.description, style: TextStyle(color: Colors.grey[900])),
+              ],
             ),
+          ),
           actions: [
-            LikeButton(
-              circleColor:
-              CircleColor(start: Color(0xffffeb3b), end: Color(0xffb71c1c)),
-              bubblesColor: BubblesColor(
-                dotPrimaryColor: Color(0xffffeb3b),
-                dotSecondaryColor: Color(0xffb71c1c),
-              ),
-              likeBuilder: (bool isLiked) {
-                return Icon(
-                  Icons.favorite_rounded,
-                  color: isLiked ? Colors.red[800] : Colors.grey,
-                );
-              },
-              likeCount: widget.markerDTO.likes?.length,
-              isLiked: _userLikedIt(),
-              onTap: onLikeButtonTapped,
-            ),
-            editButton(),
+            _likeButton(),
+            _editButton(context),
+            _commentButton(context),
           ],
         )
     );
   }
+
+  IconButton _commentButton(BuildContext context) {
+    return IconButton(
+            key: ValueKey("CommentsButton"),
+            icon : Icon(Icons.chat_bubble_outline_rounded),
+            color: Colors.black,
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return CommentsPage(markerDTO);
+              }));
+            },
+          );
+  }
+
+  IconButton _editButton(BuildContext context) {
+    if(UserService.isMarkerOwner(markerDTO.owner)) {
+      return IconButton(
+        key: ValueKey("EditButton"),
+        icon: Icon(Icons.edit_outlined),
+        color: Colors.black,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return EditMarker(markerDTO);
+          }));
+        },
+      );
+    }
+  }
+
+  LikeButton _likeButton() {
+    return LikeButton(
+            circleColor:
+            CircleColor(start: Color(0xffffeb3b), end: Color(0xffb71c1c)),
+            bubblesColor: BubblesColor(
+              dotPrimaryColor: Color(0xffffeb3b),
+              dotSecondaryColor: Color(0xffb71c1c),
+            ),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                Icons.favorite_rounded,
+                color: isLiked ? Colors.red[800] : Colors.grey,
+              );
+            },
+            likeCount: markerDTO.likes?.length,
+            isLiked: _userLikedIt(),
+            onTap: onLikeButtonTapped,
+          );
+  }
   bool _userLikedIt() {
     final bool isUserAnon = UserService.isAnonymousUser();
     final user = UserService.getCurrentUser();
-    return !isUserAnon && widget.markerDTO.likes.contains(user.uid);
+    return !isUserAnon && markerDTO.likes.contains(user.uid);
   }
 
   Future<bool> onLikeButtonTapped(bool isLiked) async{
@@ -99,13 +106,11 @@ class _MarkerIconState extends State<MarkerIcon>{
       String uid = UserService
           .getCurrentUser()
           .uid;
-      MarkerService.refreshLikes(widget.markerDTO.id, uid, isLiked);
+      MarkerService.refreshLikes(markerDTO.id, uid, isLiked);
       return !isLiked;
     }
     else{
       return isLiked;
     }
   }
-
-
 }
